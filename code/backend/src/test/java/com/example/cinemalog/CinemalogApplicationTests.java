3ddd5c1a878c942e6451ef29c0d78dfadc3e622a
Prayfon Hokseng <prayfon.h@kkumail.com>
2026-09-18 00:07:20 +0700

    feat: initialize Spring Boot Maven project

---
+++ code/backend/src/test/java/com/example/cinemalog/CinemalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
